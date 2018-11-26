8057cb531e94a3ebb3ed4283da4e9c0ead90f8a1
Luan Nguyen <S01110138@acad.tri-c.edu>
2018-11-26 14:34:30 -0500

    Lab5 completed

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* #2.
+             * Scope - (double height and double width) located in 'public ' and is anything within the method header.
+             * Static vs Non-static - static allows you to be able to access the information across the code
+             * while non-static requires you to get the information from elsewhere.
+             * Return type - (double) shows how the information will be returned.
+             * Method name (identifier) - (getArea) tells the code where to find the information.
+             * Parameters - in private,tells the code what restrictions are in place if there are any. Public does not
+             * have any restrictions.
+             * Method body - (return height) marks the area of code where it should be executed.
+             */
+
+            /* #3.
+             * User-defined methods are provided by the user while a method provided with a framekwork can be reused in 
+             * other locations. When creating a user-defined method you should consider not making it 
+             * more complicated than necessary.  */
+
+            /* #4.
+             * The difference between static and non-static methods is that in non-static 
+             * can only be accessed using objects while static methods can access static variables without any objects 
+             */
+
+            /* #5.
+             *  public string bark;
+             *  public static void vark();
+             *  {
+             *  Console.WriteLine("{0} is Barking. . ." , Name);
+             *  }
+              */
+
+            /* #6.
+             * public string doTrick;
+             * public static void doTrick(trickName);
+             * {
+             * Console.WriteLine(" {0} is so smart! {0} is doing a(n) {1}", name, trickName);
+             */
+      
+        }
+    }
+}
