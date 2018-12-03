bca5cab7d4a76222b4c87ec4b594388d97ec8c27
Luan Nguyen <S01110138@acad.tri-c.edu>
2018-12-03 18:57:30 -0500

    Completed Lab6

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* #2. A one-dimensional array p contains four elements.  The array access expressions to access each of the elements in p are 
+             * p[0], p[1], p[2], and p[3] */
+
+
+
+/* #3. 
+ *   public static void Main(string[] args)
+{
+string[] months = { "1st January", 
+                   "2nd February",
+                   "3rd march", 
+                   "4th April", 
+                   "5th May", 
+                   "6th June", 
+                   "7th July",
+                   "8th August",
+                   "9th September", 
+                   "10th October", 
+                   "11th November", 
+                   "12th December" }
+;for (int i = 0; i < 12; i++){ Console.WriteLine(months[i]); };}} */
+
+
+
+/* #4.
+ *  public static void Main(string[] args)
+{
+String[] seasons = {"Fall", "Winter", "Spring", "Summer"};
+foreach (string season in seasons)
+{
+Console.WriteLine(season);
+}
+}
+}
+}
+} */
+
+
+/* #5. 
+ * public static void Main(string[] args)
+{
+int[] randomNumber = new int[1000];
+Random random = new Random();
+for (int i = 0; i < randomNumber.Length; i++)
+{
+randomNumber[i] = random.Next(0, 100);
+}
+
+foreach (int i in randomNumber)
+{
+Console.WriteLine(i.ToString());
+}
+
+}
+}
+}
+*/
+
+/* #6. 
+ * public static void Main(string[] args)
+{
+string[] names = {"Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford"};
+int i = 0;
+while (i < names.Length)
+{
+Console.WriteLine(names[i]);
+i++;
+}
+}
+} 
+}
+*/
+
+
+}
+}
+}
