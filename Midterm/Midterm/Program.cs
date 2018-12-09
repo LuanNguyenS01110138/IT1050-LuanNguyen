f775ab9df5f0bb76d1e5f3bb0445b99a9089dde3
Luan Nguyen <S01110138@acad.tri-c.edu>
2018-12-09 13:54:37 -0500

    Completed Midterm

---
+++ Midterm/Midterm/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* #2. Loops,If-statements and switch.
+             * Loops will repeat until the desired end sequence. If-statements performs the condition if it is met and switches
+             * will replace if-statements */
+
+            /* #3. 
+            int i = 4;
+            Boolean keepLooping = true;
+            while (keepLooping)
+            {
+            if (i <= 3)
+                keepLooping = false;
+            i++;
+            Console.WriteLine(i);
+            */
+
+
+
+            /* #4. 
+              int i = 2;
+            while (i <= 128)
+            {
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine("[{0}]", i);
+                }
+                else if ((i % 2) != 0)
+                {
+                    Console.Write("");
+                }
+
+                i *= 2; */
+
+
+
+            /* #5.
+             string a = ",";
+            int b;
+            for (b = 49; b >= 1; --b)
+            {
+            Console.Write(b);
+            if (b >= 2)
+            {
+                Console.Write(a); */
+
+
+            /* #6. 
+             int a = 1;
+            while (a <=  21)
+            {
+            {
+                if ((a %2 ) == 0)
+                {
+                    Console.Write(" ");
+                }
+                else if ((a % 2) != 0)
+                {
+                    Console.Write(a);
+                }
+                a++; */
+
+
+
+            /* #7. 
+             The output for the following code is ' * '. This code replicated with while statements is nothing since
+             the do-while checks after executing while the while checks before.
+
+
+
+            /* 8. You can combine Boolean values by using boolean operators.
+                  bool icyRain = false ;
+                  bool tornadoWarning = false;
+                  if ((!icyRain) && (!tornadoWarning))
+                    {
+                        Console.WriteLine("Let's go outside!"); } */
+
+
+
+        }
+    }
+}
